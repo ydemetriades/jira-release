f3a40e493cfbe12cbc9c74fad107960d8b409a28
Nicolas M <nicolas.mesaritis@hellasdirect.gr>
2021-04-27 17:04:24 +0300

    Add first component functionality

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description:
 
-`ydemetriades/jira-release` docker image enables the creation of Release Versions in Jira.
+`ydemetriades/jira-release` docker image enables the creation of Jira Release Versions and Jira (if not exist).
 You can find it at [DockerHub](https://hub.docker.com/repository/docker/ydemetriades/ydemetriades/jira-release).
 
 ## Runtime & Tags
@@ -14,8 +14,9 @@ You can find it at [DockerHub](https://hub.docker.com/repository/docker/ydemetri
 |Tag|Version|Pull|
 |:-:|:-----:|----|
 |__v1.0__|__v1.0__|`ydemetriades/jira-release:v1.0`|
+|__v2.0__|__v2.0__|`ydemetriades/jira-release:v2.0`|
 
-_Note_: `latest` tag points to `v1.0`
+_Note_: `latest` tag points to `v2.0`
 
 ### Windows
 
@@ -24,8 +25,9 @@ _Note_: `latest` tag points to `v1.0`
 |Tag|Version|Pull|
 |:-:|:-----:|----|
 |__v1.0-win1809__|__v1.0__|`ydemetriades/jira-release:v1.0-win1809`|
+|__v2.0-win1809__|__v2.0__|`ydemetriades/jira-release:v2.0-win1809`|
 
-Note: `latest-win1809` tag points to `v1.0-win1809`
+Note: `latest-win1809` tag points to `v2.0-win1809`
 
 ## Parameters
 
@@ -34,12 +36,14 @@ Parameters are defined as environment variables
 |Name|Required|Description|Default Value|Available Options|Example|
 |:--:|:------:|-----------|-------------|-----------------|-------|
 |__JIRA_VERSION_NAME__|Yes|Release Name|-|-|v1.0|
+|__JIRA_COMPONENT_NAME__|Yes|Component Name|-|-|IntegrationService|
 |__JIRA_PROJ__|Yes|Jira Project Short Name|-|-|TES|
 |__JIRA_AUTH_USER__|Yes|Jira API Authorization Username|-|-|user|
 |__JIRA_AUTH_PASSWORD__|Yes|Jira API Authorization Password|-|-|password|
 |__JIRA_URL__|No|Jira Url|https://jira.org|-|http://jira.mydomain.com|
 |__JIRA_VERSION_RELEASED__|No|Indicates whether the current version has been released|`true`|`true`, `false`|`false`|
 |__JIRA_VERSION_DESCRIPTION__|No|Description for the current version|Version {JIRA_VERSION_NAME}|-|Fixed issue TES-101|
+|__JIRA_COMPONENT_DESCRIPTION__|No|Description for the current component|-|-|-|
 
 ## Notes
 
--- script/jira-release.py
+++ script/jira-release.py
@@ -3,10 +3,22 @@ import os
 import requests
 from datetime import datetime
 
+# Component
+jira_component_name = os.getenv('JIRA_COMPONENT_NAME')
+
+# Version
 jira_version_name = os.getenv('JIRA_VERSION_NAME')
+
+# Project
 jira_project = os.getenv('JIRA_PROJ')
+
+#Authentication
 auth_user = os.getenv('JIRA_AUTH_USER')
 auth_password = os.getenv('JIRA_AUTH_PASSWORD')
+
+if jira_component_name is None:
+    print("Component Name Variable [JIRA_COMPONENT_NAME] is not defined.")
+    exit(2)    
 
 if jira_version_name is None:
     print("Version Name Variable [JIRA_VERSION_NAME] is not defined.")
@@ -28,13 +40,14 @@ jira_version_date = datetime.today().strftime('%d/%b/%Y')
 
 jira_url = os.getenv('JIRA_URL', 'https://jira.org')
 
-jira_version_release_env = os.getenv('JIRA_VERSION_RELEASED', 'true')
+jira_version_release_env = os.getenv('JIRA_VERSION_RELEASED', 'false')
 jira_version_release = True
 
 if jira_version_release_env == 'false':
     jira_version_release = False
 
 jira_version_description =os.getenv('JIRA_VERSION_DESCRIPTION', 'Version {}'.format(jira_version_name))
+jira_component_description=os.getenv('JIRA_COMPONENT_DESCRIPTION')
 
 print('Will Attempt to create version [{}] for project [{}] '.format(jira_version_name, jira_project))
 
@@ -43,6 +56,27 @@ jira_api_version = os.getenv('JIRA_API_VERSION', '2')
 
 if jira_api_version == '2':
     data = {
+        'description': jira_component_description,
+        'name': jira_component_name,
+        'project': jira_project,        
+    }
+    
+    # Construct URL
+    api_url = ('%(url)s/rest/api/2/component' % {'url': jira_url})
+    
+    print('Sending request to:')
+    print(api_url)
+    print('with body')
+    print(componentData)
+
+    # Post build status to CI build
+    response = requests.post(api_url, auth=(auth_user, auth_password), json=componentData)
+
+    print('Response:')
+    print(response)
+    print(response.text)
+
+    data = {
         'description': jira_version_description,
         'name': jira_version_name,
         'userReleaseDate': jira_version_date,
@@ -50,7 +84,7 @@ if jira_api_version == '2':
         'released': jira_version_release
     }
 
-    # Construct URL
+    # Construct Version creation URL
     api_url = ('%(url)s/rest/api/2/version' % {'url': jira_url})
 
     print('Sending request to:')
@@ -58,7 +92,7 @@ if jira_api_version == '2':
     print('with body')
     print(data)
 
-    # Post build status to Bitbucket
+    # Post build status to CI build
     response = requests.post(api_url, auth=(auth_user, auth_password), json=data)
 
     print('Response:')

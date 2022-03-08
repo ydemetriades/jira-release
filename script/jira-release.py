f61eb4702078701368c3299ba31094049012ffb0
ydemetriades <yiannis.demetriades@gmail.com>
2022-03-08 13:36:53 +0200

    update script to support update version name

--- script/jira-release.py
+++ script/jira-release.py
@@ -6,6 +6,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 from datetime import datetime
 import json
+from requests.exceptions import HTTPError
 
 class EnvDefault(argparse.Action):
     def __init__(self, envvar, required=False, default=None, **kwargs):
@@ -19,42 +20,33 @@ class EnvDefault(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         setattr(namespace, self.dest, values)
 
-### Required Arguments
 parser = argparse.ArgumentParser( description='Create Jira versions easily', epilog="And that's how we create Jira versions easily!")
+
+### Required Arguments
 parser.add_argument('--version', '-v', action=EnvDefault, envvar='JIRA_VERSION_NAME', type=str, required=True, help='The unique name of the version. Can be specified by environment variable JIRA_VERSION_NAME.')
 parser.add_argument('--project', '-p', action=EnvDefault, envvar='JIRA_PROJ', type=int, required=True, help='The ID of the project to which this version is attached.')
 parser.add_argument('--user', '-u', action=EnvDefault, envvar='JIRA_AUTH_USER', type=str, required=True, help='The Jira authentication user email.')
 parser.add_argument('--password', action=EnvDefault, envvar='JIRA_AUTH_PASSWORD', type=str, required=True, help='Jira API Authorization Password / API Token.')
 
 ### Optional Arguments
-parser.add_argument('--description', '-d', action=EnvDefault, envvar='JIRA_VERSION_DESCRIPTION', default='', type=str, help='The description of the version. Default value is an empty string.')
+parser.add_argument('--description', '-d', action=EnvDefault, envvar='JIRA_VERSION_DESCRIPTION', default=None, type=str, help='The description of the version. Default value is an empty string.')
 
 # Should update instead of create
-help_update='Indicates whether to update or create the version. Can be specified by environment variable JIRA_VERSION_UPDATE. Default value is false.'
-if "JIRA_VERSION_UPDATE" in os.environ:
-    parser.add_argument('--update', type=bool, default=os.environ.get("JIRA_VERSION_UPDATE"), help=help_update)
-else:
-    parser.add_argument('--update', action='store_true', help=help_update)
+parser.add_argument('--update', action=EnvDefault, envvar='JIRA_VERSION_UPDATE', type=bool, default=False, help='Indicates whether to update or create the version. Can be specified by environment variable JIRA_VERSION_UPDATE. Default value is false.')
+
+parser.add_argument('--new-version', action=EnvDefault, envvar='JIRA_NEW_VERSION_NAME', type=str, help='The new name of the version to be updated. Can be specified by environment variable JIRA_NEW_VERSION_NAME.')
 
 # Released
-help_released='Indicates that the version is released. Can be specified by environment variable JIRA_VERSION_RELEASED. Default value is false.'
-if "JIRA_VERSION_RELEASED" in os.environ:
-    parser.add_argument('--released', type=bool, default=os.environ.get("JIRA_VERSION_RELEASED"), help=help_released)
-else:
-    parser.add_argument('--released', action='store_true', help=help_released)
+parser.add_argument('--released', action=EnvDefault, envvar='JIRA_VERSION_RELEASED', type=bool, help='Indicates that the version is released. Can be specified by environment variable JIRA_VERSION_RELEASED. Default value is false.')
 
 # Archived
-help_archived='Indicates that the version is archived. Can be specified by environment variable JIRA_VERSION_ARCHIVED. Default value is false.'
-if "JIRA_VERSION_ARCHIVED" in os.environ:
-    parser.add_argument('--archived', type=bool, default=os.environ.get("JIRA_VERSION_ARCHIVED"), help=help_archived)
-else:
-    parser.add_argument('--archived', action='store_true', help=help_archived)
+parser.add_argument('--archived', action=EnvDefault, envvar='JIRA_VERSION_ARCHIVED', type=bool, help='Indicates that the version is archived. Can be specified by environment variable JIRA_VERSION_ARCHIVED. Default value is false.')
 
 # Url
 parser.add_argument('--url', action=EnvDefault, envvar='JIRA_URL', type=str, default='https://jira.org', help='The Jira host url. Default value will be https://jira.org')
 
 # API Version
-help_api_version='The Jira API version. Default value is 3. Can be specified by environment variable JIRA_API_VERSION. Supports only version 2 and 3.'
+help_api_version='The Jira API version. Default value is 3. Can be specified by environment variable JIRA_API_VERSION. Supports only versions 2 and 3.'
 if "JIRA_API_VERSION" in os.environ:
     parser.add_argument('--api-version', type=int, default=os.environ.get("JIRA_API_VERSION"), help=help_api_version)
 else:
@@ -74,39 +66,84 @@ headers = {
 }
 
 data = {
-    'description': args.description,
-    'name': args.version,
-    'projectId': args.project,
-    'released': args.released,
-    "archived": args.archived,
+    'projectId': args.project
 }
 
-# If released
-if args.released:
-    data['userReleaseDate'] = jira_version_date
+# Include description
+if args.description != None:
+    data['description'] = args.description
+
+# Include released
+if args.released != None:
+    data['released'] = args.released
+    # If released
+    if args.released:
+        data['userReleaseDate'] = jira_version_date
+
+# Include archived
+if args.archived != None:
+    data['archived'] = args.archived
+
+restMethod = "POST"
 
 # Construct URL
 if args.update:
-    api_url = ('%(url)s/rest/api/%(api_version)s/version/%(version)s' %{'url': args.url, 'api_version': args.api_version, 'version': args.version})
+    # Update needs to be a PUT request
+    restMethod = "PUT"
+
+    # Fetch Version Id
+    params = {
+        'query': args.version,
+        'maxResults': 1
+    }
+
+    getVersionIdUrl = ('%(url)s/rest/api/%(api_version)s/project/%(project)s/version' %{'url': args.url, 'api_version': args.api_version, 'project': args.project})
+
+    try:
+        response = requests.request(
+            "GET",
+            getVersionIdUrl,
+            params=params,
+            headers=headers,
+            auth=auth
+        )
+        response.raise_for_status()
+        # access Json content
+        jsonResponse = response.json()
+        if jsonResponse["values"] and jsonResponse["values"][0] and jsonResponse["values"][0]["id"]:
+            versionId = jsonResponse["values"][0]["id"]
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
+    if versionId == None:
+        exit('Unable to retrieve version ID for %(version)s. \nUsage:\n %(usage)s' %{'version': args.version, 'usage': parser.print_usage()})
+
+    if args.new_version != None:
+        data['name'] = args.new_version
+    api_url = ('%(url)s/rest/api/%(api_version)s/version/%(version)s' %{'url': args.url, 'api_version': args.api_version, 'version': versionId})
 else:
+    data['name'] = args.version
     data['startDate'] = jira_version_date
     api_url = ('%(url)s/rest/api/%(api_version)s/version' %{'url': args.url, 'api_version': args.api_version})
 
-payload = json.dumps(data)
+try:
+    response = requests.request(
+        restMethod,
+        api_url,
+        data=json.dumps(data),
+        headers=headers,
+        auth=auth
+    )
 
-print('\nSending request:')
-print(api_url)
-print('\nBODY:')
-print(data)
-
-response = requests.request(
-   "POST",
-   api_url,
-   data=payload,
-   headers=headers,
-   auth=auth
-)
-
-print('\nResponse:')
-print(response)
-print(response.text)
+    print('\nResponse:')
+    if response.ok:
+        print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+    else:
+        print("Oops! Something went wrong.")
+        print(response)
+except HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')
+except Exception as err:
+    print(f'Other error occurred: {err}')
